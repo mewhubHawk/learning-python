289a0cf1960d39666bb2682a88dc174ee2e5c77f
K <34939082+mewhubHawk@users.noreply.github.com>
2024-10-10 11:09:45 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,31 @@
+print("jingfei chen\n")
+#comment
+
+"""
+multi 
+line 
+comment
+"""
+
+print("these are my favourites")
+singer = "jingfei chen"
+fashionStyle = "lolita"
+colours = "pink and blue"
+#readable variable names make a kind programmer
+print("my favourite singer is",singer)
+print("my favourite fashion style is",fashionStyle)
+print("my favourite colours are",colours )
+#lowerCamelCase is what i prefer to use
+
+'''
+one may reasign the variables' values but the previous 
+value cant be used after unless it is reassigned again
+'''
+
+singer = "haruna"
+fashionStyle = "gyaru"
+colours = "green and blue"
+print("\nmy favourite singer is",singer)
+print("my favourite fashion style is",fashionStyle)
+print("my favourite colours are",colours)
+#\n is used to create a new line
